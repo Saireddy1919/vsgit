cd1570e1a7b5fcf42a7cea01c8e37d870ebc2ec5
Saireddy1919 <52202429+Saireddy1919@users.noreply.github.com>
2019-06-26 16:42:35 +0530

    Update Operation.cs
    modified

--- WebApplication1/WebApplication1/Models/Operation.cs
+++ WebApplication1/WebApplication1/Models/Operation.cs
@@ -7,5 +7,13 @@ namespace WebApplication1.Models
 {
     public class Operation
     {
+        public int Adition(int x, int y)
+        {
+            int a=10;
+            int b=20;
+            int c=a+b;
+            return c;
+        }
+
     }
 }
